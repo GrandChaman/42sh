df277bdfc1f43db80c0fbdfdbf205fcdc63637f2
rfautier <rfautier@student.42.fr>
2018-04-11 17:44:55 +0200

    no leaks but CTRLC + hererdoc == shit

--- srcs/lexer_parser_ast/ast/tree_build/heredoc_node.c
+++ srcs/lexer_parser_ast/ast/tree_build/heredoc_node.c
@@ -14,7 +14,7 @@
 #include "ast.h"
 #include "ft_sh.h"
 
-static int		final_heredoc(char *tmp, char *str, char *tmp2, int fd)
+static int		final_heredoc(char *tmp, char *str, char *tmp2, int fd, t_sh21 *sh21)
 {
 	while ((ft_strcmp(str, tmp) != 0) && tmp[0] != T_CTRL_D)
 	{
@@ -25,6 +25,7 @@ static int		final_heredoc(char *tmp, char *str, char *tmp2, int fd)
 		if ((tmp = read_command(NULL, 0, 1, 0)) == NULL)
 		{
 			free(str);
+			sh21->signal = SIGNAL_CTRLC;
 			return (-1);
 		}
 	}
@@ -50,7 +51,9 @@ void		heredoc_node(t_ast_node *node)
 	int		fd;
 	char	*tmp2;
 	char	*tmp_file;
+	t_sh21	*sh21;
 
+	sh21 = sh21_get();
 	str = node->content;
 	if (!(tmp_file = random_str(SIZE_RANDOM_STR)))
 		return ;
@@ -58,16 +61,18 @@ void		heredoc_node(t_ast_node *node)
 	if ((tmp = read_command(NULL, 0, 1, 0)) == NULL)
 	{
 		free(str);
+		sh21->signal = SIGNAL_CTRLC;
 		return ;
 	}
 	tmp2 = ft_itoa(1);
 	path_file = ft_strjoin(TMP_PATH_HEREDOC, tmp_file);
+	free(tmp_file);
 	free(tmp2);
 	if ((fd = open(path_file, O_RDWR | O_CREAT | O_EXCL |
 		O_APPEND, 0777)) == -1)
 		open_heredoc_file(path_file, &fd);
 	free(path_file);
-	if ((final_heredoc(tmp, str, tmp2, fd)) == -1)
+	if ((final_heredoc(tmp, str, tmp2, fd, sh21)) == -1)
 		return ;
 	close(fd);
 	return ;
--- srcs/main.c
+++ srcs/main.c
@@ -50,12 +50,14 @@ int			main(void)
 		//@param	int status : La valeur de retour de la derniere commande
 		//@param	heredoc : Si le prompt actuel est un heredoc (Laisser *prompt a NULL si heredoc est egale a 1)
 		//@param	fb : (boolean) Si le CLI doit afficher un feedback (Genre OK en vert ou Stop en rouge)
-		cmd = read_command(NULL, 0, 0, (!fb ? fb++ : fb));
-		sh21->buf = cmd;
+		if (((cmd = read_command(NULL, 0, 0, (!fb ? fb++ : fb))) == NULL))
+			break ;
+		sh21->buf = cmd; // a free lexa->str;
 		lexer(sh21);
 		if (parser(sh21->lex) && sh21->signal != SIGNAL_CTRLC)
 			sh21_get()->ret = exec_tree(sh21->tree.root_node);
 		add_to_history(shell, cmd); // ADD TO HISTORY
+		del_sh21();
 		free(cmd);
 	}
 	cli_loader(1); //1 = destroying the cli
