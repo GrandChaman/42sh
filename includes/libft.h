2bfc69e3c51a43d68db235e30b223915e3797dd9
Frederic BERTOIA <fbertoia@e1r6p21.42.fr>
2018-04-16 17:58:29 +0200

    last

--- includes/libft.h
+++ includes/libft.h
@@ -236,5 +236,6 @@ int						ft_asprintf(char **ret, const char *format, ...);
 int						ft_snprintf(char *ret, size_t size, const char *format,
 	...);
 int						ft_stralike(char *str1, char *str2, size_t len);
+char					*ft_strjoin_multiple(int nb_args, ...);
 
 #endif
