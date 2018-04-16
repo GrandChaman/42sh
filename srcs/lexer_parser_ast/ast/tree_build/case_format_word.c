3472c8eb00c561184d58eba0c956407db2e903d7
Unknown <frederic.bertoia@gmail.com>
2018-04-17 00:57:16 +0200

    ebauche

--- srcs/lexer_parser_ast/ast/tree_build/case_format_word.c
+++ srcs/lexer_parser_ast/ast/tree_build/case_format_word.c
@@ -14,11 +14,23 @@
 
 void	case_backslash(char **ret, char **ptr, int *i, char *special_chars)
 {
+	t_ast	*tree;
+	int		len;
+
+	tree = &sh21_get()->tree;
 	*ret = add_str(ret, ptr, i);
 	(*i)++;
 	if (!special_chars || ft_strindex(special_chars, (*ptr)[*i]) >= 0)
 		(*ptr)++;
 	*ret = add_str(ret, ptr, i);
+	ft_printf("le charactere courant |%s|\n", *ptr);
+	if (**ptr == '\'' || **ptr == '\"')
+	{
+		len = ft_strlen(*ret);
+		tree->escaped_quote_count[tree->nb_escaped_quote] = len ? len - 1 : len;
+		ft_printf("charactere escape : '%c' index %d \n", **ptr, tree->escaped_quote_count[tree->nb_escaped_quote]);
+		tree->nb_escaped_quote++;
+	}
 }
 
 void	case_dollar(char **ret, char **ptr, int *i)
