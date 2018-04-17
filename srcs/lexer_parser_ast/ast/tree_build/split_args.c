ccdf9f70ddba978419f280651efc6a0eba8a321b
Unknown <frederic.bertoia@gmail.com>
2018-04-17 17:02:05 +0200

    fix issue \"

--- srcs/lexer_parser_ast/ast/tree_build/split_args.c
+++ srcs/lexer_parser_ast/ast/tree_build/split_args.c
@@ -11,56 +11,7 @@
 /* ************************************************************************** */
 
 #include "sh21.h"
-// static int		get_next_quote(char *argv, char quote)
-// {
-// 	int		i;
-//
-// 	i = 1;
-// 	while (argv[i] && argv[i] != quote)
-// 		i++;
-// 	if (argv[i])
-// 		i++;
-// 	return (i);
-// }
-//
-// static void		case_whitespace(char **argv, int *wd_nb, int *flag)
-// {
-// 	while (is_whitespace(**argv))
-// 		(*argv)++;
-// 	if (*flag)
-// 		(*wd_nb)++;
-// 	*flag = 0;
-// }
-//
-// static int		word_number(char *argv)
-// {
-// 	int		wd_nb;
-// 	int		flag;
-// 	int 	array_quote;
-//
-// 	flag = 0;
-// 	wd_nb = 0;
-// 	array_quote = 0;
-// 	while (*argv)
-// 	{
-// 		if (*argv == (char)'\'' || *argv == (char)'\"')
-// 		{
-// 			argv += get_next_quote(argv, *argv);
-// 			flag = 1;
-// 		}
-// 		else if (is_whitespace(*argv))
-// 			case_whitespace(&argv, &wd_nb, &flag);
-// 		else
-// 		{
-// 			while (!is_whitespace(*argv) && ft_isprint(*argv) && *argv
-// 			!= (char)'\'' && *argv != (char)'\"')
-// 				argv++;
-// 			flag = 1;
-// 		}
-// 	}
-// 	return (wd_nb + flag);
-// }
-//
+
 char			*word(char **argv)
 {
 	char	*ret;
@@ -88,42 +39,14 @@ char			*word(char **argv)
 	*argv += i;
 	return (ret);
 }
-//
-// char			**split_args(char *argv)
-// {
-// 	char	**ret;
-// 	int		wd_nb;
-// 	int		i;
-// 	char	**u;
-//
-// 	i = 0;
-// 	wd_nb = word_number(argv);
-// 	if (!(ret = (char**)malloc(sizeof(char*) * (wd_nb + 1))))
-// 	{
-// 		ft_error(errno, "malloc");
-// 		return (NULL);
-// 	}
-// 	while (*argv && i < wd_nb)
-// 	{
-// 		while (*argv && is_whitespace(*argv))
-// 			argv++;
-// 		ret[i++] = word(&argv);
-// 	}
-// 	ret[i] = NULL;
-// 	u = ret;
-// 	while (*u)
-// 		++u;
-// 	return (ret);
-// }
 
 int escaped_quote(char *argv, t_ast *tree, int *i, int *j)
 {
 	if (argv[*i + *j] == '\'' || argv[*i + *j] == '\"')
 	{
-		if (tree->i < tree->nb_escaped_quote && tree->esc_i[tree->i] == (*i + *j + tree->quote_count))
+		if (tree->i < tree->nb_escaped_quote && tree->esc_i[tree->i]
+			== (*i + *j + tree->quote_count))
 		{
-			// ft_printf("*************escaped quote********** tree->nb_escaped_quote = %d, tree->esc_i[tree->i] = %d  tree->i = %d, i + j = %d, tree->quote = %d\n",
-			// tree->nb_escaped_quote, tree->esc_i[tree->i], tree->i, *i + *j, tree->quote_count);
 			tree->i++;
 			return (1);
 		}
@@ -144,7 +67,8 @@ void case_quote_args(char *argv, int *i, int *j)
 	{
 		ft_strcpy(argv + *i + *j, argv + *i + *j + 1);
 		tree->quote_count += 1;
-		while (argv[*i + *j] && (argv[*i + *j] != c || escaped_quote(argv, tree, i, j)))
+		while (argv[*i + *j] && (argv[*i + *j] != c
+		|| escaped_quote(argv, tree, i, j)))
 			*j += 1;
 		ft_strcpy(argv + *i + *j, argv + *i + *j + 1);
 		tree->quote_count += 1;
@@ -201,9 +125,6 @@ char **copy_list_to_array(t_args **list)
 	}
 	ret[i] = NULL;
 	del_list((void**)list, del_el);
-	// i = 0;
-	// while (ret[i] && ft_printf("arg[%d] = |%s|\n", i, ret[i]))
-	// 	i++;
 	return (ret);
 }
 
@@ -220,8 +141,9 @@ char **split_args(char *argv)
 	sh21_get()->tree.quote_count = 0;
 	while (argv[i + j])
 	{
-		// ft_printf("argv = %s, i = %d, j = %d\n", argv + i + j, i, j);
-		if (argv[i + j] == '\"' || argv[i + j] == '\'')
+		if (escaped_quote(argv, &sh21_get()->tree, &i, &j))
+			j++;
+		else if (argv[i + j] == '\"' || argv[i + j] == '\'')
 			case_quote_args(argv, &i, &j);
 		else if (is_whitespace(argv[i + j]))
 			case_space(argv, &i, &j, &list);
