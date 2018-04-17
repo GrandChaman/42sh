88bed62822974807f1d85298942d1f070e86f510
Francis LE ROY <francis.leroy.ch@gmail.com>
2018-04-17 13:26:08 +0200

    Autocomplete working with hash table

--- libft/libft/ft_strndup2.c
+++ libft/libft/ft_strndup2.c
@@ -6,22 +6,23 @@
 /*   By: hfontain <hfontain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/16 12:41:49 by hfontain          #+#    #+#             */
-/*   Updated: 2018/04/16 15:14:37 by hfontain         ###   ########.fr       */
+/*   Updated: 2018/04/17 13:19:02 by fle-roy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-char	*ft_strndup2(char *str, size_t size)
+char	*ft_strndup2(char *str, size_t n)
 {
-	char *tmp;
+	char	*nstr;
+	size_t	len;
 
-	if (!str || size <= 0)
+	if (!str || !n)
 		return (NULL);
-	tmp = (char *)malloc(sizeof(char) * size);
-	if (tmp == NULL)
-		exit(0);
-	ft_strlcpy(tmp, str, size);
-	return (tmp);
+	len = ft_strlen(str);
+	len = (len > n ? n : len);
+	nstr = ft_strnew(n);
+	ft_strncpy(nstr, str, n);
+	return (nstr);
 }
