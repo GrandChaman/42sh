2b4275e37084ffd7c570885dda8a11f2db8941b8
Francis LE ROY <francis.leroy.ch@gmail.com>
2018-04-18 16:03:25 +0200

    Added more options to history

--- srcs/builtins/bi_history.c
+++ srcs/builtins/bi_history.c
@@ -6,7 +6,7 @@
 /*   By: fle-roy <fle-roy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/17 16:55:16 by fle-roy           #+#    #+#             */
-/*   Updated: 2018/04/18 13:28:29 by fle-roy          ###   ########.fr       */
+/*   Updated: 2018/04/18 16:02:47 by fle-roy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include "cli.h"
 #include <time.h>
 
-static int			display_history(int limit)
+static int			display_history(int lim)
 {
 	t_ft_sh			*sh;
 	t_list			*tmp;
@@ -24,11 +24,13 @@ static int			display_history(int limit)
 
 	sh = get_ft_shell();
 	i = 0;
-	if (!(tmp = (limit || limit > ft_lstsize(sh->history) ?
-		ft_lstat(sh->history, limit) : ft_lstlast(sh->history))))
+	if (lim < 0 || lim > sh->history_size)
+		return (ft_fprintf(2, "42sh: history: %d: index out of range\n", lim)
+			&& 1);
+	if (!(tmp = (lim ? ft_lstat(sh->history, lim) : ft_lstlast(sh->history))))
 		return (0);
 	if (tmp != sh->history)
-		i = ft_lstsize(tmp);
+		i = sh->history_size;
 	while ((tmp = tmp->prev))
 	{
 		entry = (t_ft_hist_entry*)tmp->content;
@@ -38,6 +40,65 @@ static int			display_history(int limit)
 		ft_printf("%d\t%s\t%s\n", i, t_format, entry->command);
 		i++;
 	}
+	return (0);
+}
+
+static int			clear_history(void)
+{
+	t_ft_sh *sh;
+
+	sh = get_ft_shell();
+	ft_lstdel(&sh->history, delete_hist_entry);
+	sh->history_size = 0;
+	return (0);
+}
+
+static int			read_and_append_history_form_file(char *path)
+{
+	t_ft_sh		*sh;
+	int			fd;
+
+	sh = get_ft_shell();
+	if ((fd = open(path, O_RDONLY)) < 0)
+		return (ft_fprintf(2, "42sh: history: can't read file %s.\n", path)
+			&& 1);
+	if (read_history(sh, fd) < 0)
+	{
+		close(fd);
+		return (ft_fprintf(2, "42sh: history: error while reading %s.\n", path)
+			&& 1);
+	}
+	close(fd);
+}
+
+static int			write_history_to_file(char *path)
+{
+	t_ft_sh		*sh;
+	int			fd;
+
+	sh = get_ft_shell();
+	if ((fd = open(path, O_WRONLY)) < 0)
+		return (ft_fprintf(2, "42sh: history: can't write to file %s.\n", path)
+			&& 1);
+	write_history(sh, fd, 0);
+	close(fd);
+	return (0);
+}
+
+static int			delete_at_offset(int offset)
+{
+	t_ft_sh	*sh;
+	int		len;
+	t_list	*tmp;
+
+	sh = get_ft_shell();
+	len = sh->history_size;
+	if (offset < 0 || len - offset < 0)
+		return (ft_fprintf(2, "42sh: history: %d: index out of range\n",
+			offset) && 1);
+	if (!(tmp = ft_lstat(sh->history, len - offset)))
+		return (1);
+	ft_lstdelone(&tmp, delete_hist_entry);
 	return (0);
 }
 
@@ -56,7 +117,7 @@ int					bi_history(int argc, char **argv, char ***environ)
 		while (argv[1][i])
 			if (!ft_isdigit(argv[1][i++]))
 				return (ft_fprintf(2, "42sh: history: %s: numeric argument"
-					" required", argv[1]) && 1);
+					" required\n", argv[1]) && 1);
 		return (display_history(ft_atoi(argv[1])));
 	}
 
