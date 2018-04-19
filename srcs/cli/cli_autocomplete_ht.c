3c7ce36a33a919abe60ca155c7198a9c6350b335
Francis LE ROY <francis.leroy.ch@gmail.com>
2018-04-19 18:28:02 +0200

    Autocomplete deals with builtins

--- srcs/cli/cli_autocomplete_ht.c
+++ srcs/cli/cli_autocomplete_ht.c
@@ -6,7 +6,7 @@
 /*   By: fle-roy <fle-roy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/21 16:29:28 by fle-roy           #+#    #+#             */
-/*   Updated: 2018/04/17 13:19:51 by fle-roy          ###   ########.fr       */
+/*   Updated: 2018/04/19 18:24:51 by fle-roy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,27 @@ static void		insert_ht_el_in_list(t_list **list,
 	ft_lstpush_back(list, &entry, sizeof(t_ft_autoc_entry));
 }
 
+static void		add_builtin_to_ht(t_list **list, char *str_part)
+{
+	int					i;
+	size_t				len;
+	t_ft_autoc_entry	entry;
+
+	len = ft_strlen(str_part);
+	i = 0;
+	while (g_builtins[i].fn_ptr)
+	{
+		if (!ft_stralike(str_part, (char*)g_builtins[i].fn_name, len))
+		{
+			entry.undeline = 1;
+			entry.color = ANSI_COLOR_B_CYAN;
+			entry.name = ft_strdup(g_builtins[i].fn_name);
+			ft_lstpush_back(list, &entry, sizeof(t_ft_autoc_entry));
+		}
+		i++;
+	}
+}
+
 void			collect_data_ht(t_list **list, char *str_part)
 {
 	unsigned int		i;
@@ -41,16 +62,14 @@ void			collect_data_ht(t_list **list, char *str_part)
 	size_t				len;
 
 	lexer_s = sh21_get();
-	if (!lexer_s || !str_part)
+	if (!lexer_s || !str_part || (i = 0))
 		return ;
-	i = 0;
 	len = ft_strlen(str_part);
 	hash_el = &(lexer_s->env.hash_table[i]);
 	while (i < HASH_SIZE)
 	{
-		if (!(bin_name = get_el_name(hash_el)))
+		if (!(bin_name = get_el_name(hash_el)) && ++i)
 		{
-			i++;
 			hash_el = &(lexer_s->env.hash_table[i]);
 			continue ;
 		}
@@ -60,4 +79,5 @@ void			collect_data_ht(t_list **list, char *str_part)
 			continue ;
 		i++;
 	}
+	add_builtin_to_ht(list, str_part);
 }
