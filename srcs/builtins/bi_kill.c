1516698f921cd103e857bfa23cda947ef341ffda
rfautier <rfautier@student.42.fr>
2018-04-23 15:19:46 +0200

    pause

--- srcs/builtins/bi_kill.c
+++ srcs/builtins/bi_kill.c
@@ -2,9 +2,14 @@
 
 int					bi_kill(int argc, char **argv, char ***environ, t_ast_node *root)
 {
-	(void)argc;
-	(void)argv;
+	int i;
+
 	(void)environ;
 	(void)root;
+	i = 1;
+	if (argc == 1)
+	{
+		ft_fprintf(2, "kil")
+	}
 	return (0);
 }
