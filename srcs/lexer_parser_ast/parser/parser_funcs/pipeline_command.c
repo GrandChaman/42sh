f0e6381e1bd1c5d042c462aef217eebbf95156ef
Hugo FONTAINE <fontaine_hugo@orange.fr>
2018-04-24 16:32:44 +0200

    Fixed time

--- srcs/lexer_parser_ast/parser/parser_funcs/pipeline_command.c
+++ srcs/lexer_parser_ast/parser/parser_funcs/pipeline_command.c
@@ -6,7 +6,7 @@
 /*   By: hfontain <hfontain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/12 17:59:42 by fbertoia          #+#    #+#             */
-/*   Updated: 2018/03/12 18:04:05 by hfontain         ###   ########.fr       */
+/*   Updated: 2018/04/24 16:32:11 by hfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,7 @@ t_lex	*pipeline_command(t_lex *lex)
 	if (!assert_lex(lex))
 		return (g_err_lex);
 	if (assert_lex((tmp = is_token(lex, Bang))))
-	{
 		lex = tmp;
-		if (assert_lex((tmp = timespec(lex))))
-			lex = tmp;
-	}
-	else if (assert_lex((tmp = timespec(lex))))
-	{
-		lex = tmp;
-		if (assert_lex((tmp = is_token(lex, Bang))))
-			lex = tmp;
-	}
 	lex = pipeline(lex);
 	return (lex);
 }
--- srcs/lexer_parser_ast/parser/parser_funcs/timespec.c
+++ srcs/lexer_parser_ast/parser/parser_funcs/timespec.c
@@ -6,7 +6,7 @@
 /*   By: hfontain <hfontain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/12 17:59:42 by fbertoia          #+#    #+#             */
-/*   Updated: 2018/03/12 18:03:37 by hfontain         ###   ########.fr       */
+/*   Updated: 2018/04/24 16:28:36 by hfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_lex	*timespec(t_lex *lex)
 {
 	if (!assert_lex(lex))
 		return (g_err_lex);
-	if (assert_lex(lex) && ft_strequ(lex->content, "-p"))
-		return ((lex->next));
+	//if (assert_lex(lex) && ft_strequ(lex->content, "-p"))
+	//	return ((lex->next));
 	return (lex);
 }
