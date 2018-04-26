aee01a12476afb4f6b3505e366f28cba77ee8628
Unknown <frederic.bertoia@gmail.com>
2018-04-26 16:21:15 +0200

    fix $

--- srcs/lexer_parser_ast/ast/tree_build/case_format_word.c
+++ srcs/lexer_parser_ast/ast/tree_build/case_format_word.c
@@ -45,17 +45,22 @@ void	case_dollar(char **ret, char **ptr, int *i)
 	int		end;
 
 	end = 0;
-	*ret = add_str(ret, ptr, i);
-	(*ptr)++;
-	if (ft_strnequ(*ptr, "((", 2))
-	{
-		*ret = ft_strffjoin(*ret, ft_itoa(ft_eval_expr(*ptr, &end, 0)));
-		*ptr += end;
-	}
+	if (!(*ptr)[*i + 1] || is_whitespace((*ptr)[*i + 1]))
+		*i += 1;
 	else
 	{
-		*ret = ft_strffjoin(*ret, find_var(*ptr));
-		*ptr += skip_var(*ptr);
+		*ret = add_str(ret, ptr, i);
+		(*ptr)++;
+		if (ft_strnequ(*ptr, "((", 2))
+		{
+			*ret = ft_strffjoin(*ret, ft_itoa(ft_eval_expr(*ptr, &end, 0)));
+			*ptr += end;
+		}
+		else
+		{
+			*ret = ft_strffjoin(*ret, find_var(*ptr));
+			*ptr += skip_var(*ptr);
+		}		
 	}
 }
 
