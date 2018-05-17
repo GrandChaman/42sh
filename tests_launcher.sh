ef3cc31825a21c4171833a5779025b2037e1b74c
Francis LE ROY <francis.leroy.ch@gmail.com>
2018-05-17 20:44:51 +0200

    Added some tests

--- tests_launcher.sh
+++ tests_launcher.sh
@@ -5,13 +5,16 @@ NC="\033[0m"
 
 function check_if_ok {
 eval $1
-if [ $? -ne 0 ]
+last_val=$?
+if [ $last_val -ne 0 ]
 then
 	echo -e "$> $1" $RED " KO " "$NC"
-	exit 1
+	exit $last_val
 else
 	echo -e "$> $1" $GREEN " OK " "$NC"
 fi
 }
 
 check_if_ok "diff <(./42sh test/basics) <(bash -- test/basics)"
+check_if_ok "diff <(./42sh test/builtins) <(bash -- test/builtins)"
+check_if_ok "diff <(./42sh test/path) <(cat test/path_correct)"
